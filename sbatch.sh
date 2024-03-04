ef71a36f43b2751b4b2162767ec30adb8743ada7
Qi Hou <qhou4@jhu.edu>
2024-03-04 11:36:09 -0500

    change sbatch

--- sbatch.sh
+++ sbatch.sh
@@ -2,12 +2,12 @@
 
 #SBATCH --partition=mig_class
 #SBATCH --gres=gpu:1
-#SBATCH --nodes=2
-#SBATCH --ntasks-per-node=2
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
 #SBATCH --time=2:00:0
 #SBATCH --job-name="CS 601.471/671 homework6 3.1.2"
 #SBATCH --output=slurm-%j.out
-#SBATCH --mem=32G
+#SBATCH --mem=16G
 
 # add module only when you run this code on rockfish
 module load anaconda 
@@ -15,4 +15,4 @@ source ~/.bashrc
 conda activate ssm_hw6 # activate the Python environment
 
 # runs your code
-python base_classification.py --device cuda --model "distilbert-base-uncased" --batch_size "101" --lr 1e-4 --num_epochs 30
+python base_classification.py --device cuda --model "distilbert-base-uncased" --batch_size "101" --lr 1e-4 --num_epochs 7
